70c8ef763b7c4746325d8b4ea0c4483b150fb5cd
Séba <seba@seba.gent>
2025-04-18 23:02:29 +0200

    Update config_flow.py

--- custom_components/ehealth_status/config_flow.py
+++ custom_components/ehealth_status/config_flow.py
@@ -1,1 +1,9 @@
+from homeassistant import config_entries
+from .const import DOMAIN
+
+class EHealthConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="eHealth Status", data={})
+        return self.async_show_form(step_id="user")
 
