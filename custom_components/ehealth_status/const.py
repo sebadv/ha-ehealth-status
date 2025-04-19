61122b63ec3db3e50ab2014c7a3348316e158d65
Séba <seba@seba.gent>
2025-04-19 17:15:41 +0200

    Update const.py
    Multiple URL's depending on language
    
    Signed-off-by: Séba <seba@seba.gent>

--- custom_components/ehealth_status/const.py
+++ custom_components/ehealth_status/const.py
@@ -1,2 +1,3 @@
 DOMAIN = "ehealth_status"
-API_URL = "https://status.ehealth.fgov.be/nl/api/components/prod"
+API_URL_NL = "https://status.ehealth.fgov.be/nl/api/components/prod"
+API_URL_FR = "https://status.ehealth.fgov.be/fr/api/components/prod"
